bd504b706f8c787595e008c56792ebfe7e063f55
BD <BD@172.19.142.138>
2017-12-09 13:07:26 -0600

    Changed login controller

--- DCS/src/main/java/mc/controller/LoginController.java
+++ DCS/src/main/java/mc/controller/LoginController.java
@@ -1,6 +1,7 @@
 package mc.controller;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -9,23 +10,48 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import mc.dao.DoctorDAO;
+import mc.model.Doctor;
 import mc.model.User;
+import mc.service.DoctorService;
 import mc.service.UserService;
 
 @Controller
 public class LoginController {
 	
+	private HttpSession session;
+	
 	@Autowired
     private UserService userService;
+	@Autowired
+    private DoctorService doctorService;
 	
 	@RequestMapping(value = "/login", method = RequestMethod.POST)
-    public String login(Model model, String username,String password) {
+    public String login(Model model, String username,String password, HttpServletRequest request) {
+		session = request.getSession();
 		User user = userService.findByUserNameAndPwd(username, password);
 		if(null == user) {
             model.addAttribute("error", "Your username and password is invalid.");
 			return "login";
 		}else {
-			return "redirect:/patientHome?uid="+user.getId();
+			System.out.println(user.getSetting().getVarValue());
+			
+			if(user.getSetting().getVarValue().equals("Patient")) {
+				session.setAttribute("Patient", user);
+				return "redirect:/patientHome?uid="+user.getId();
+			}
+				
+			if(user.getSetting().getVarValue().equals("Doctor")) {
+				Doctor doctor = doctorService.findDoctorByUser(user);
+				session.setAttribute("Doctor", doctor);
+				return "doctorHome";//TODO doctor home.jsp
+			}
+				
+			if(user.getSetting().getVarValue().equals("Receptionist")) {
+				session.setAttribute("Receptionist", user);
+				return "reception/receptionHome";//TODO reception home.jsp
+			}
+			return "error";
 		}
     }
 	
@@ -35,3 +61,4 @@ public class LoginController {
 	}
 
 }
+	
